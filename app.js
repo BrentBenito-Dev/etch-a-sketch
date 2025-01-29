c3a4d5e844c0fff5d6b91cd4b209cd864301435e
NeoooDev <124891218+BrentBenito-Dev@users.noreply.github.com>
2025-01-29 21:09:46 +0800

    feat: hover effect and grid size input

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // CREATE grid
-const grid = document.querySelector("#grid")
+const grid = document.querySelector("#grid");
+const DEFAULT_GRID_SIZE = 16 * 16;
+const gridSizeForm = document.querySelector("#gridSizeForm");
+let gridSize;
 
-function gridSquare(){
-    const gridSquarePixel = document.createElement("div");
-    gridSquarePixel.setAttribute("class", "grid-square-pixel") 
-    gridSquarePixel.style.border = ".1em solid black";  
-    gridSquarePixel.style.padding = ".75rem"
-    gridSquarePixel.style.flex = "1 0 0%";
-    grid.appendChild(gridSquarePixel);
-}
+gridSizeForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const formData = new FormData(gridSizeForm);
+    gridSize = formData.get("gridSizeNum") * formData.get("gridSizeNum");
+    drawGrid();
+});
+
 function drawGrid(){
-    for (let i = 1; i <= 256;i++){
-       gridSquare(); 
+    if (gridSize == undefined){
+        gridSize = DEFAULT_GRID_SIZE;
     }
+    for (let i = 1; i <= gridSize ;i++){
+        const gridSquarePixel = document.createElement("div");
+        gridSquarePixel.setAttribute("class", "grid-square-pixel"); 
+        gridSquarePixel.setAttribute("id", "square");
+    
+        gridSquarePixel.addEventListener("mouseover", () =>{
+            gridSquarePixel.style.background = "black";
+        }); 
+
+        gridSquarePixel.style.width = "100%"
+        grid.appendChild(gridSquarePixel);
+    }  
 }
-
-
-
-drawGrid();
