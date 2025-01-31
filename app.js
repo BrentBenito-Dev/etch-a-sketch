e2c3ff0fdabffcd49708b320a54c3123790e83a4
NeoooDev <124891218+BrentBenito-Dev@users.noreply.github.com>
2025-01-31 20:48:32 +0800

    fix: grid squares are not evenly drawn

--- app.js
+++ app.js
@@ -7,24 +7,45 @@ let gridSize;
 gridSizeForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const formData = new FormData(gridSizeForm);
-    gridSize = formData.get("gridSizeNum") * formData.get("gridSizeNum");
-    drawGrid();
+    gridSize = formData.get("gridSizeNum");
+    if (gridSize != 0){
+        clearGrid();
+    }
+
+    if(gridSize < 100){
+        drawGrid(gridSize);
+    }else{
+        alert("The grid is too big!")
+    }
 });
 
-function drawGrid(){
+function drawGrid(size){
     if (gridSize == undefined){
         gridSize = DEFAULT_GRID_SIZE;
     }
-    for (let i = 1; i <= gridSize ;i++){
+    for (let i = 1; i <= size * size;i++){
         const gridSquarePixel = document.createElement("div");
         gridSquarePixel.setAttribute("class", "grid-square-pixel"); 
         gridSquarePixel.setAttribute("id", "square");
-    
+        grid.appendChild(gridSquarePixel);
+        
         gridSquarePixel.addEventListener("mouseover", () =>{
             gridSquarePixel.style.background = "black";
         }); 
-
-        gridSquarePixel.style.width = "100%"
-        grid.appendChild(gridSquarePixel);
+    
     }  
+    const gridSquareSize = 100 / size;
+    const gridSquareStyle = `width: ${gridSquareSize}%; height: ${gridSquareSize}%;`;
+    const gridSquares = document.querySelectorAll(".grid-square-pixel")
+    gridSquares.forEach(square => {
+        square.style.cssText = gridSquareStyle;
+    });
+
+   
+}
+
+function clearGrid(){
+    while(grid.hasChildNodes()){
+        grid.removeChild(grid.firstChild);
+    }
 }
